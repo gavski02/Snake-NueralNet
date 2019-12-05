75120bf9fe67ed9c0a9a47eac64dadf9cd0f1106
Gavin Bowen <gavinbowen@pop-os.localdomain>
2019-12-04 21:58:19 -0800

    init

--- js/main.js
+++ js/main.js
@@ -1,12 +1,54 @@
+let snake;
+let s = 10;
+let food;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    w = floor(640/s);
+    h = floor(480/s);
+    createCanvas(w*s, h*s);
+    snake = new Snake();
+    frameRate(7);
+    pickLocation();
 }
+
 
 function draw() {
-    background(255);
-    text("put your p5.js code here",10, frameCount % height);
+    background(40);
+
+    if (snake.eat(food)) {
+        pickLocation();
+    }
+
+    snake.death();
+    snake.update();
+    snake.show();
+
+    fill(255, 0, 100);
+    rect(food.x, food.y, s, s);
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+function keyPressed() {
+    if (keyCode == 87) {
+        snake.dir(0,-1);
+    }
+    else if (keyCode == 83) {
+        snake.dir(0, 1);
+    }
+    else if (keyCode == 65) {
+        snake.dir(-1, 0);
+    }
+    else if (keyCode == 68) {
+        snake.dir(1, 0);
+    }
 }
+
+function pickLocation() {
+  let cols = floor(width/s);
+  let rows = floor(height/s);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(s);
+}
+
+
+
+
